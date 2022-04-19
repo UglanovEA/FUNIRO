df158236e2a0e6be06ac4e279ea3dadee2b31aa1
UglanovEA <UglanovEA@gmail.com>
2022-04-19 14:26:11 +0300

    ver 2.3

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -25,4 +25,16 @@ window.onload = function () {
 			document.querySelector(".search-form").classList.remove("_active");
 		}
 	}
+
+	// Фиксированная шапка
+	const headerElement = document.querySelector(".header");
+	const callback = function (entries, observer) {
+		if (entries[0].isIntersecting) {
+			headerElement.classList.remove("_scroll");
+		} else {
+			headerElement.classList.add("_scroll");
+		}
+	};
+	const headerObserver = new IntersectionObserver(callback);
+	headerObserver.observe(headerElement);
 };
