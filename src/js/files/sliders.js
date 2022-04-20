1179205ebf4a2724e749512195ca82663db1d378
UglanovEA <UglanovEA@gmail.com>
2022-04-20 10:47:21 +0300

    ver 2.6

--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -96,6 +96,75 @@ function initSliders() {
 			on: {},
 		});
 	}
+	if (document.querySelector(".slider-rooms__body")) {
+		// Указываем скласс нужного слайдера
+		// Создаем слайдер
+		new Swiper(".slider-rooms__body", {
+			// Указываем скласс нужного слайдера
+			// Подключаем модули слайдера
+			// для конкретного случая
+			modules: [Navigation, Pagination, Parallax],
+			observer: true,
+			observeParents: true,
+			slidesPerView: "auto",
+			spaceBetween: 24,
+			watchOverflow: true,
+			speed: 800,
+			parallax: true,
+			//touchRatio: 0,
+			//simulateTouch: false,
+			loop: true,
+			loopAdditionalSlides: 5,
+			preloadImages: false,
+			//lazy: true,
+
+			/*
+			// Эффекты
+			effect: 'fade',
+			autoplay: {
+				delay: 3000,
+				disableOnInteraction: false,
+			},
+			*/
+
+			// Пагинация
+			pagination: {
+				el: ".slider-rooms__dotts",
+				clickable: true,
+			},
+
+			// Кнопки "влево/вправо"
+			navigation: {
+				prevEl: ".slider-rooms .slider-arrow_prev",
+				nextEl: ".slider-rooms .slider-arrow_next",
+			},
+
+			// Брейкпоинты
+			/*
+			breakpoints: {
+				320: {
+					slidesPerView: 1,
+					spaceBetween: 0,
+					autoHeight: true,
+				},
+				768: {
+					slidesPerView: 2,
+					spaceBetween: 20,
+				},
+				992: {
+					slidesPerView: 3,
+					spaceBetween: 20,
+				},
+				1268: {
+					slidesPerView: 4,
+					spaceBetween: 30,
+				},
+			},
+			*/
+			// События
+			on: {},
+		});
+	}
 }
 // Скролл на базе слайдера (по классу swiper_scroll для оболочки слайдера)
 function initSlidersScroll() {
